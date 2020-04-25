82790926b15d021f82280df5eb746a1bf78b57b9
Joosep Sisas <joosep.sisas@mil.ee>
2020-04-25 20:48:38 +0300

    HTTP logging

---
+++ src/main/java/com/konto/konto/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,18 @@
+package com.konto.konto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+}
